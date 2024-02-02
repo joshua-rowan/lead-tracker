1f6185889251ba03888c1ccd9fe480ccd31cdd4b
Josh <joshua.rowan.2021@gmail.com>
2024-02-02 12:42:07 -0600

    added dblclick delete all btn

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if (leadsFromLocalStorage) {
+    for (i=0; i<leadsFromLocalStorage.length; i++)
+    myLeads.push(leadsFromLocalStorage[i])
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", event =>{
+    localStorage.removeItem("myLeads")
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
